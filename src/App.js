a14bd733de7af9a9f26c78c6d5444e5e9426ea3e
hazemmu <hazeeemm5566@gmail.com>
2022-02-21 11:49:39 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App () {
+  return (
+    <div >
+      <h2>let's start coding</h2>
+      <p>this is also visible</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import reactDOM from "react-dom";
+
+import './index.js';
+import App from './App';
+
+reactDOM.render(<App></App>, document.getElementById('root'));
